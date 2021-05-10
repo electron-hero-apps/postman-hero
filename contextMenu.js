d3b5b6d7a5c19b9ab5deefc9130b1ce3c742cabf
John Anderson <andersjj@yahoo.com>
2021-05-10 18:51:26 -0500

    initial commit

---
+++ contextMenu.js
@@ -0,0 +1,196 @@
+var currentEditableItem;
+var oldText;
+var currentListItem;
+
+const {
+	remote
+} = require('electron')
+const {
+	Menu,
+	MenuItem
+} = remote
+
+const contextMenu = new Menu()
+
+contextMenu.append(new MenuItem({
+	label: 'New Collection',
+	click() {
+		// var filePath = localStorage.getItem('currentPath');
+		// var fileContent = "";
+		// var filename = "untitled.txt";
+		// fs.writeFile(path.join(filePath, filename), fileContent, (err) => {
+		// 	if (err) throw err;
+		// 	buildTreeView('#files', localStorage.getItem('currentPath'))
+		// });
+		treeViewJSON.items.push({
+			name: 'New Collection',
+			id: 'folder-1',
+			type: 'folder',
+			data: {},
+			folderChildren: []
+		});
+		localStorage.setItem('treeView', JSON.stringify(treeViewJSON));
+		$('#files').empty();
+		buildTreeViewFromJSON('#files', treeViewJSON)
+
+	}
+}))
+
+
+
+const fileContextMenu = new Menu();
+
+fileContextMenu.append(new MenuItem({
+	label: 'Rename',
+	click() {
+		console.log('here in rename click');
+		$(currentListItem).attr('contenteditable', true);
+		oldText = $(currentListItem).html();
+		$(currentListItem).focus();
+	}
+}));
+
+fileContextMenu.append(new MenuItem({
+	label: 'Delete',
+	click() {
+		if (confirm("Are you sure?") === true) {
+			//var filePath = localStorage.getItem('currentPath');
+			var filePath = $(currentListItem).data('path');
+
+			fs.unlink(filePath, function(err, data) {
+				if (err) {
+					alert(err)
+				}
+				buildTreeView('#files', localStorage.getItem('currentPath'))
+			});
+		}
+	}
+}));
+
+
+const folderContextMenu = new Menu();
+
+folderContextMenu.append(new MenuItem({
+	label: 'Rename',
+	click() {
+		console.log('here in rename click');
+		$(currentListItem).attr('contenteditable', true);
+		oldText = $(currentListItem).html();
+		$(currentListItem).focus();
+	}
+}));
+
+folderContextMenu.append(new MenuItem({
+	label: 'Delete',
+	click() {
+		if (confirm("Are you sure?") === true) {
+			var filePath = localStorage.getItem('currentPath');
+			var filename = $(currentListItem).html();
+			fs.unlink(path.join(filePath, filename), function(err, data) {
+				if (err) {
+					alert(err)
+				}
+				buildTreeView('#files', localStorage.getItem('currentPath'))
+			});
+		}
+	}
+}));
+
+// folderContextMenu.append(new MenuItem({
+// 	label: 'New File',
+// 	click() {
+// 		console.log('new file...');
+// 		var folderPath = $(currentListItem).data('path');
+// 		console.log(folderPath);
+// 		var fileContent = 'no information yet';
+// 		var filename = "untitled.txt";
+// 		fs.writeFile(path.join(folderPath, filename), fileContent, (err) => {
+// 			if (err) throw err;
+// 			buildTreeView('#files', localStorage.getItem('currentPath'))
+// 		});
+// 	}
+// }));
+//
+// folderContextMenu.append(new MenuItem({
+// 	label: 'New Folder',
+// 	click() {
+// 		var folderPath = $(currentListItem).data('path');
+// 		fs.mkdir(path.join(folderPath, 'New Folder'), function(err, data){
+// 			console.log(err);
+// 			console.log('created');
+// 		});
+// 	}
+// }));
+
+
+window.addEventListener('contextmenu', (e) => {
+    console.log('here in context');
+	e.preventDefault();
+	var x = e.clientX;
+	var y = e.clientY;
+	var el = document.elementFromPoint(x, y);
+	if ($(el).hasClass('nav-group-collections')) {
+		currentListItem = el;
+		contextMenu.popup({
+			window: remote.getCurrentWindow()
+		})
+	}
+
+	// if ($(el).hasClass('file-item')) {
+	// 	currentListItem = el;
+	// 	fileContextMenu.popup({
+	// 		window: remote.getCurrentWindow()
+	// 	})
+	// }
+	//
+	if ($(el).hasClass('folder-item')) {
+		currentListItem = el;
+		folderContextMenu.popup({
+			window: remote.getCurrentWindow()
+		})
+
+		var index = treeViewJSON.items.indexOf($(el).data('folderJSON'));
+		treeViewJSON.items[index].name = 'Renamed Folder';
+
+		localStorage.setItem('treeView', JSON.stringify(treeViewJSON));
+		$('#files').empty();
+		buildTreeViewFromJSON('#files', treeViewJSON)
+	}
+
+}, false)
+
+
+// function handleFileItemRightClick(event) {
+// 	console.log('here in right click')
+// 	currentEditableItem = $(this);
+// 	$(this).attr('contenteditable', true);
+// 	oldText = $(this).html();
+// 	$(this).focus();
+// 	event.stopPropagation();
+// }
+
+function handleFileItemKeyDown(event) {
+	if (event.keyCode === 13 || event.keyCode === 27) {
+
+		if (event.keyCode === 27) {
+			$(currentListItem).html(oldText);
+		}
+
+		if (event.keyCode === 13) {
+			// hit enter, go ahead and rename file
+			// var oldPath = $(this).data('path');
+			// newPath = oldPath.split(path.sep);
+			// newPath.pop();
+			// newPath = newPath.join(path.sep);
+			// newPath = path.join(newPath,$(currentListItem).html())
+			// fs.rename(oldPath, newPath, function(err, data) {
+			// 	console.log(err);
+			// 	console.log('here in rename callback')
+			// })
+		}
+
+		$(currentListItem).removeAttr('contenteditable');
+		$(currentListItem).blur();
+
+	}
+}
---
+++ index.js
@@ -0,0 +1,88 @@
+const ipc = require('electron')
+	.ipcRenderer
+let nodeRequirePath = ipc.sendSync('getRequirePath') + 'node_modules/';
+window.$ = window.jQuery = require(nodeRequirePath + 'jquery');
+var path = require(nodeRequirePath + 'path');
+var fs = require('fs');
+var _ = require(nodeRequirePath + 'lodash');
+var Client = require('ssh2')
+	.Client;
+
+var treeViewJSON;
+
+
+$(document)
+	.ready(function() {
+		$(document)
+			.on('click', '.icon.opener', handleOpenerClick);
+		$(document)
+			.on('click', '.nav-item-text', handleItemClick);
+		$(document)
+			.on('click', '.clickable-nav-item', handleFileItemClick);
+			$(document).on('keydown', '.folder-item', handleFileItemKeyDown);
+
+
+		// var treeViewJSON = {
+			// items: [{
+			// 	name: 'Servers',
+			// 	id: 'folder-1',
+			// 	type: 'folder',
+			// 	data: {},
+			// 	folderChildren: [{
+			// 		name: 'new Folder',
+			// 		type: 'folder'
+			// 	}, {
+			// 		name: 'Raspberry Pi 1',
+			// 		id: 'file-1',
+			// 		type: 'file',
+			// 		extraInfo: {
+			// 			ipAddress: '',
+			// 			username: '',
+			// 			password: ''
+			// 		}
+			// 	}, {
+			// 		name: 'Raspberry Pi 2',
+			// 		id: 'file-2',
+			// 		type: 'file',
+			// 		extraInfo: {
+			// 			ipAddress: '',
+			// 			username: '',
+			// 			password: ''
+			// 		}
+			// 	}]
+			// }, {
+			// 	name: 'Packages',
+			// 	id: 'folder-2',
+			// 	type: 'folder',
+			// 	folderChildren: [{
+			// 		name: 'Disk Utils',
+			// 		type: 'folder',
+			// 		folderChildren: [{
+			// 			name: 'List',
+			// 			id: 'file-1',
+			// 			type: 'file',
+			// 			extraInfo: {
+			// 				packageDir: 'diskUtils',
+			// 				startPage: 'diskUtils.html'
+			// 			},
+			// 		}]
+			// 	}]
+			// }, {
+			// 	name: 'Three',
+			// 	id: 'folder-3',
+			// 	type: 'folder',
+			// 	data: {}
+			// }]
+
+		// }
+		treeViewJSON = JSON.parse(localStorage.getItem('treeView'));
+		if (!treeViewJSON) {
+			console.log('defaulting items');
+			treeViewJSON = {
+				items: []
+			}
+		}
+		buildTreeViewFromJSON('#files', treeViewJSON)
+		//buildTreeView('#files', __dirname);
+
+	});
